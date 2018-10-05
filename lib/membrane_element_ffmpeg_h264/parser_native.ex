2799e68e41a826899ac47b8f045c55d90e17f3a6
Bartosz Błaszków <bartosz.blaszkow@swmansion.com>
2018-10-05 13:16:36 +0200

    Remove some leftovers

--- lib/membrane_element_ffmpeg_h264/parser_native.ex
+++ lib/membrane_element_ffmpeg_h264/parser_native.ex
@@ -1,10 +1,3 @@
 defmodule Membrane.Element.FFmpeg.H264.Parser.Native do
   use Unifex.Loader
-
-  def foo(pframes, input) do
-    Enum.map_reduce(pframes, input, fn size, stream ->
-      <<frame::bytes-size(size), rest::binary>> = stream
-      {frame, rest}
-    end)
-  end
 end
