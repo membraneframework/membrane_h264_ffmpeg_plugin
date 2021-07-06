b7f354d542a3e5f4e61e9cbd29c550b618736f3b
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2021-07-06 12:01:31 +0200

    Reorder deps

--- mix.exs
+++ mix.exs
@@ -57,17 +57,17 @@ defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
   defp deps do
     [
       {:bunch, "~> 1.3.0"},
-      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.24", only: :dev, runtime: false},
       {:membrane_caps_video_h264, "~> 0.2.0"},
       {:membrane_caps_video_raw, "~> 0.1.0"},
       {:membrane_common_c, "~> 0.7.0"},
       {:membrane_core, "~> 0.7.0"},
-      {:membrane_element_rawvideo_parser, "~> 0.4.0", only: [:dev, :test]},
-      {:membrane_file_plugin, "~> 0.6.0", only: [:dev, :test]},
       {:ratio, "~> 2.4.0"},
-      {:unifex, "~> 0.4.0"}
+      {:unifex, "~> 0.4.0"},
+      {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.0", only: [:dev, :test], runtime: false},
+      {:membrane_element_rawvideo_parser, "~> 0.4.0", only: [:dev, :test]},
+      {:membrane_file_plugin, "~> 0.6.0", only: [:dev, :test]}
     ]
   end
 end
