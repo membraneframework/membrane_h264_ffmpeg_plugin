2c8f44de0f095ff9b5bb2762652d664f85d3fbac
Dominik Artur Wołek <dominik.wolek@swmansion.com>
2022-05-13 15:23:50 +0200

    Prepare for releasing

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.20.0"
+  @version "0.20.1"
   @github_url "https://github.com/membraneframework/membrane_h264_ffmpeg_plugin"
 
   def project do
