03009a1c42f82e2dbd1ee4532e646db4e530d99f
Jakub Perżyło <qizot1405@gmail.com>
2022-07-19 10:18:15 +0200

    Testing

--- c_src/membrane_h264_ffmpeg_plugin/parser.c
+++ c_src/membrane_h264_ffmpeg_plugin/parser.c
@@ -58,7 +58,8 @@ static int resolution_changed(resolution last_res, State *state) {
 void update_last_frame_number(int poc, State *state) {
   state->last_frame_number += 1;
   if(poc == 0) {
-    state->poc_offset += state->last_frame_number;
+    // frame gets incremented once while poc gets incremented twice?
+    state->poc_offset = 2 * state->last_frame_number;
   }
 }
 
