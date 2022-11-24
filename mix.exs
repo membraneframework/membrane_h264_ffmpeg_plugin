d476f3d7c2af66b157b4c4029de8e618cca8d3a0
Łukasz Wala <lukasz.wala@swmansion.com>
2022-11-24 17:43:42 +0100

    Bump version after merge

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.24.0"
+  @version "0.25.0"
   @github_url "https://github.com/membraneframework/membrane_h264_ffmpeg_plugin"
 
   def project do
