5b493bd4fb5aec5ea599b8bc9b09aec9518e9b8b
varsill <lukasz.kita0@gmail.com>
2023-09-08 16:29:09 +0200

    revoke the precompiled os deps for ffmpeg

--- bundlex.exs
+++ bundlex.exs
@@ -7,9 +7,9 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         {:precompiled,
          "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n4.4-latest-linux64-gpl-shared-4.4.tar.xz/"}
 
-      # %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-      #   {:precompiled,
-      #    "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/version1/ffmpeg_macos_intel_with_brew.tar.gz"}
+      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
+        {:precompiled,
+         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/version1/ffmpeg_macos_intel_with_brew.tar.gz"}
 
       _other ->
         nil
