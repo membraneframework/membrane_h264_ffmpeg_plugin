88e284a86245bb1ae4afe2fbf5a3eed9b375ec5b
varsill <lukasz.kita0@gmail.com>
2023-09-19 16:14:43 +0200

    update links

--- bundlex.exs
+++ bundlex.exs
@@ -5,11 +5,11 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
     case Bundlex.get_target() do
       %{os: "linux"} ->
         {:precompiled,
-         "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n4.4-latest-linux64-gpl-shared-4.4.tar.xz/"}
+         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/version1/ffmpeg_linux.tar.gz"}
 
       %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
         {:precompiled,
-         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/version1/ffmpeg_macos_intel_with_brew.tar.gz"}
+         "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/download/version1/ffmpeg_macos_intel.tar.gz"}
 
       _other ->
         nil
