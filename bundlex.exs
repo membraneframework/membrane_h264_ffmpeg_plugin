fe6973b73fb7f0c9f508f9fcd40e6505f3dd1933
Noarkhh <jakub.pryc@gmail.com>
2023-12-08 19:54:37 +0100

    Stop downloading precompiled deps for alpine

--- bundlex.exs
+++ bundlex.exs
@@ -1,26 +1,41 @@
 defmodule Membrane.H264.FFmpeg.BundlexProject do
   use Bundlex.Project
 
-  defp get_ffmpeg_url() do
+  defp get_ffmpeg() do
     membrane_precompiled_url_prefix =
       "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg"
 
-    case Bundlex.get_target() do
-      %{architecture: "aarch64", os: "linux"} ->
-        "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linuxarm64-gpl-shared-6.0.tar.xz"
+    membrane_precompiled_url =
+      case Bundlex.get_target() do
+        %{os: "linux", vendor: "alpine"} ->
+          nil
 
-      %{os: "linux"} ->
-        "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"
+        %{architecture: "aarch64", os: "linux"} ->
+          "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linuxarm64-gpl-shared-6.0.tar.xz"
 
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"
+        %{os: "linux"} ->
+          "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"
 
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"
+        %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
+          "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"
 
-      _other ->
-        nil
-    end
+        %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
+          "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"
+
+        _other ->
+          nil
+      end
+
+      if membrane_precompiled_url != nil do
+        [
+          {:precompiled, get_ffmpeg_url(), ["libavcodec", "libavutil"]},
+          {:pkg_config, ["libavcodec", "libavutil"]}
+        ]
+      else
+        [
+          {:pkg_config, ["libavcodec", "libavutil"]}
+        ]
+      end
   end
 
   def project() do
@@ -35,10 +50,7 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         interface: :nif,
         sources: ["decoder.c"],
         os_deps: [
-          ffmpeg: [
-            {:precompiled, get_ffmpeg_url(), ["libavcodec", "libavutil"]},
-            {:pkg_config, ["libavcodec", "libavutil"]}
-          ]
+          ffmpeg: get_ffmpeg()
         ],
         preprocessor: Unifex
       ],
@@ -46,10 +58,7 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         interface: :nif,
         sources: ["encoder.c"],
         os_deps: [
-          ffmpeg: [
-            {:precompiled, get_ffmpeg_url(), ["libavcodec", "libavutil"]},
-            {:pkg_config, ["libavcodec", "libavutil"]}
-          ]
+          ffmpeg: get_ffmpeg()
         ],
         preprocessor: Unifex
       ]
