032991e757da6d9572df32797b791899436933f8
Noarkhh <jakub.pryc@gmail.com>
2023-12-12 12:34:39 +0100

    Start using Precompiled Dependency Provider

--- bundlex.exs
+++ bundlex.exs
@@ -1,28 +1,6 @@
 defmodule Membrane.H264.FFmpeg.BundlexProject do
   use Bundlex.Project
 
-  defp get_ffmpeg_url() do
-    membrane_precompiled_url_prefix =
-      "https://github.com/membraneframework-precompiled/precompiled_ffmpeg/releases/latest/download/ffmpeg"
-
-    case Bundlex.get_target() do
-      %{architecture: "aarch64", os: "linux"} ->
-        "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linuxarm64-gpl-shared-6.0.tar.xz"
-
-      %{os: "linux"} ->
-        "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-n6.0-latest-linux64-gpl-shared-6.0.tar.xz"
-
-      %{architecture: "x86_64", os: "darwin" <> _rest_of_os_name} ->
-        "#{membrane_precompiled_url_prefix}_macos_intel.tar.gz"
-
-      %{architecture: "aarch64", os: "darwin" <> _rest_of_os_name} ->
-        "#{membrane_precompiled_url_prefix}_macos_arm.tar.gz"
-
-      _other ->
-        nil
-    end
-  end
-
   def project() do
     [
       natives: natives()
@@ -36,7 +14,8 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         sources: ["decoder.c"],
         os_deps: [
           ffmpeg: [
-            {:precompiled, get_ffmpeg_url(), ["libavcodec", "libavutil"]},
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+             ["libavcodec", "libavutil"]},
             {:pkg_config, ["libavcodec", "libavutil"]}
           ]
         ],
@@ -47,7 +26,8 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         sources: ["encoder.c"],
         os_deps: [
           ffmpeg: [
-            {:precompiled, get_ffmpeg_url(), ["libavcodec", "libavutil"]},
+            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+             ["libavcodec", "libavutil"]},
             {:pkg_config, ["libavcodec", "libavutil"]}
           ]
         ],
