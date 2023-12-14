18f736f7af90cc81e23370f6bb3a93821a2a5fae
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2023-12-14 11:16:41 +0100

    Start using Precompiled Dependency Provider (#106)
    * Start using Precompiled Dependency Provider

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.31.3"
+  @version "0.31.4"
   @github_url "https://github.com/membraneframework/membrane_h264_ffmpeg_plugin"
 
   def project do
@@ -40,7 +40,8 @@ defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
     [
       {:bunch, "~> 1.6"},
       {:bundlex, "~> 1.3"},
-      {:unifex, "~> 1.1.0"},
+      {:unifex, "~> 1.1"},
+      {:membrane_precompiled_dependency_provider, "~> 0.1.0"},
       {:membrane_core, "~> 1.0"},
       {:membrane_common_c, "~> 0.16.0"},
       {:membrane_h264_format, "~> 0.6.1"},
