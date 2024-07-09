e87a7d0f9d78eb34805f16b828c86e59a1c62188
Łukasz Kita <lukasz.kita0@gmail.com>
2024-07-09 16:23:29 +0200

    Bump to v0.32.0

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
   use Mix.Project
 
-  @version "0.31.8"
+  @version "0.32.0"
   @github_url "https://github.com/membraneframework/membrane_h264_ffmpeg_plugin"
 
   def project do
