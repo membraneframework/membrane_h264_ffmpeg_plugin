897817b1939ccae4875839f9e787f1c39993cc4e
Jakub Pryc <94321002+Noarkhh@users.noreply.github.com>
2025-10-01 13:04:26 +0200

    Update dependency provider (#118)

--- bundlex.exs
+++ bundlex.exs
@@ -14,7 +14,8 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         sources: ["decoder.c"],
         os_deps: [
           ffmpeg: [
-            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+            {:precompiled,
+             Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg, version: "6.0.1"),
              ["libavcodec", "libavutil"]},
             {:pkg_config, ["libavcodec", "libavutil"]}
           ]
@@ -26,7 +27,8 @@ defmodule Membrane.H264.FFmpeg.BundlexProject do
         sources: ["encoder.c"],
         os_deps: [
           ffmpeg: [
-            {:precompiled, Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg),
+            {:precompiled,
+             Membrane.PrecompiledDependencyProvider.get_dependency_url(:ffmpeg, version: "6.0.1"),
              ["libavcodec", "libavutil"]},
             {:pkg_config, ["libavcodec", "libavutil"]}
           ]
