f3f0132d1b44e713d00eca4d1cd6e4781f3a0ec6
feliks.pobiedzinski@swmansion.com <feliks.pobiedzinski@swmansion.com>
2026-06-29 12:12:34 +0200

    Rename prepend_llms_links to append_llms_links

--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
       source_url: @github_url,
       homepage_url: "https://membraneframework.org",
       docs: docs(),
-      aliases: [docs: ["docs", &prepend_llms_links/1]]
+      aliases: [docs: ["docs", &append_llms_links/1]]
     ]
   end
 
@@ -94,7 +94,7 @@ defmodule Membrane.H264.FFmpeg.Plugin.MixProject do
     ]
   end
 
-  defp prepend_llms_links(_) do
+  defp append_llms_links(_args) do
     output_dir = docs()[:output] || "doc"
     path = Path.join(output_dir, "llms.txt")
 
